b94fc55219791c57025767888a55aef2cd53bb8f
pp19dd <pp19dd@gmail.com>
2016-07-12 14:05:29 -0400

    Used to define data sources, behavior

---
+++ config.sample.php
@@ -0,0 +1,16 @@
+<?php
+// scores obtained from a gdoc-data output
+define( "DATA_URL", "http://example.com/?project=example&callback=?" );
+
+
+// default mode of operation is fetch via JSONP
+// uncomment to make server fetch, if the page is
+// behind a caching proxy (ex: varnish-cache)
+
+# define( "SERVER_FETCH", true );
+
+
+// images are hosted ... somewhere
+// uncomment to inline images as data:base65 instead
+
+# define( "INLINE_IMAGES", true );
